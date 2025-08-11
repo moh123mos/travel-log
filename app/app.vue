a20cc576a9365ab064baceb1240d96b2decd4d7e
moh123mos <moh162534aaa@gmail.com>
2025-08-11 23:53:17 +0300

    :sparkles: add landing page

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline mb-10">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      Secondary <Icon name="tabler:map" size="5rem" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In with github
+        <Icon name="tabler:brand-github" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style></style>
