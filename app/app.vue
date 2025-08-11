8972f3562cbeda465f295d696f1fe1fafeaf1102
Mohammed Mostafa <116978549+moh123mos@users.noreply.github.com>
2025-08-11 23:10:53 +0300

    :sparkles: add lint workflow (#48)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline mb-10">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">
+      Secondary <Icon name="tabler:map" size="5rem" />
+    </button>
   </div>
 </template>
