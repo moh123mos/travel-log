fe0bcd22d55fa307af099e8e1224c4f01b078213
moh123mos <moh162534aaa@gmail.com>
2025-08-21 22:33:14 +0300

    :sparkles: implement GitHub authentication and replace sign-in buttons with a unified BtnsAuthBtn component

---
+++ app/components/btns/auth-btn.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { useAuthStore } from "~/stores/auth";
+
+const { loading, signIn } = useAuthStore();
+</script>
+
+<template>
+  <button
+    class="btn btn-accent"
+    :disabled="loading"
+    @click="signIn"
+  >
+    Sign In with github
+    <span
+      v-if="loading"
+      class="loading loading-spinner loading-md"
+    />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+    />
+  </button>
+</template>
