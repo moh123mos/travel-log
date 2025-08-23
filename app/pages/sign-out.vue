dd109a5c63bd5d5d8206b7bd32f795ca8fdb91bc
moh123mos <moh162534aaa@gmail.com>
2025-08-24 02:50:38 +0300

    :sparkles: enhance auth UI with user avatar, sign-out

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(async () => {
+  await authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mx-auto mt-4 min-h-72 flex justify-center items-center">
+    <span class="loading loading-dots loading-xl" />
+  </div>
+</template>
