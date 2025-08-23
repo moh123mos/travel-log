603a37524c30044824a4f3a6c09b0ab7dc64c30e
Mohammed Mostafa <116978549+moh123mos@users.noreply.github.com>
2025-08-24 02:28:15 +0300

    :bug: refactor authentication handling to use unified env variables

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -6,13 +6,12 @@ export const useAuthStore = defineStore("authStore", () => {
   const loading = ref(false);
   async function signIn() {
     loading.value = true;
-    const data = await authClient.signIn.social({
+    await authClient.signIn.social({
       provider: "github",
       callbackURL: "/dashboard",
       errorCallbackURL: "/error",
     });
     loading.value = false;
-    console.warn(data);
   }
 
   return {
