0f5d914a5f285014f06a03f7d79de02122b7913c
Mohammed Mostafa <116978549+moh123mos@users.noreply.github.com>
2025-08-25 19:19:20 +0300

    ✨ implement sidebar with toggle functionality (#58)

---
+++ app/components/btns/sidebar-btn.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  href: string;
+  label: string;
+  icon: string;
+  showLabel: boolean;
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="props.href"
+    class="flex items-center gap-2 text-nowrap"
+    :class="{ 'justify-center': !props.showLabel, 'justify-start': props.showLabel }"
+  >
+    <Icon
+      size="24"
+      :name="props.icon"
+    />
+    {{ props.showLabel ? props.label : '' }}
+  </NuxtLink>
+</template>
